91b377c4b5036eb9d82a6c6f69d87b70d8d51d24
Axiomware Systems Inc <algov3@gmail.com>
2020-10-07 14:33:23 -0700

    Intial release

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * Netrunr GAPI V3 Client library
+ *
+ * Copyright(C) 2020 Axiomware Systems Inc..
+ * 
+ *  See LICENSE for more information
+ */
+
+var gapiClient = require("./lib/gapi-v3-client.js");
+
+// Expose gapiClient
+module.exports.gapiClient = gapiClient
